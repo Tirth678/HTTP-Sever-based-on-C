93a82c3a1c0be9c0af57aa86b37d00c6b1f18cbc
Tirth S Pandya <teamfznltd@gmail.com>
2024-07-11 11:32:55 +0530

    commit

--- index.c
+++ index.c
@@ -3,11 +3,40 @@
 #include<sys/stat.h>
 #include<sys/socket.h>
 #include<arpa/inet.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<string.h>
 #define PORT 8000
 #define MAXIMUM_REQUET_SIZE 2048
 #define ROOT "./src"
 void handling(int sock){
-    char 
+    char request[MAXIMUM_REQUET_SIZE] = {0};
+    recv(sock, request, MAXIMUM_REQUET_SIZE, 0);
+
+    char method[10], path[25], protocol[20];
+    sscanf(request, "%s%s%s", method, path, protocol);
+    char filePath[225];
+    sprintf(filePath, "%s%s", ROOT, path);
+    if(strcmp(path, "/") == 0){
+        sprintf(filePath, "%s/index.html", ROOT);
+    }
+    int file = open(filePath, O_RDONLY);
+    if(file == -1){
+        char res[] = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+        send(sock, res, strlen(res), 0);
+    } else{
+        char res[] = "HTTP/1.1 2000 OK\r\n\r\n";
+        send(sock, res, strlen(res), 0);
+
+        char buf[1024];
+        ssize_t readBytes;
+        while((readBytes = read(file, buf, sizeof(buf))) > 0){
+            send(sock, buf, readBytes, 0);
+        }
+        close(file);
+    }
+    close(sock);
+
 }
 int main(){
     int ServerSock, clientSock;
